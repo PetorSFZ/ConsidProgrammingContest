17dc0c91e64864af47b9a28716aea7ffd795e9ef
Peter Hillerström <peter@hstroem.se>
2016-10-28 15:55:14 +0200

    Smarter

--- src/OptimizedSmartAlgorithm2.cpp
+++ src/OptimizedSmartAlgorithm2.cpp
@@ -57,10 +57,8 @@ static __declspec(noalias) size_t readFile(const char* __restrict path, char* __
 
 bool optimizedSmartAlgorithm2(const char* filePath) noexcept
 {
-	// Read file
-	size_t tmpSize = sizeOfFile(filePath) + 16; // +16 so we guarantee SIMD writable
-	char* fileContent = static_cast<char*>(_aligned_malloc(tmpSize, 32));
-	size_t fileContentSize = readFile(filePath, fileContent);
+	size_t numFileBytes = sizeOfFile(filePath);
+	size_t numCodes = numFileBytes / 8;
 
 	// Allocate bitset buffer for whether a number is found or not, and clear it
 	const size_t NUM_BITSET_BYTES = 2197000;
@@ -70,44 +68,51 @@ bool optimizedSmartAlgorithm2(const char* filePath) noexcept
 	// Variable containing whether a copy was found or not
 	bool foundCopy = false;
 
-	// Go through file
-	for (size_t i = 5; i < fileContentSize; i += 8) {
-		char no1 = fileContent[i];
-		char no2 = fileContent[i-1];
-		char no3 = fileContent[i-2];
-		char let1 = fileContent[i-3];
-		char let2 = fileContent[i-4];
-		char let3 = fileContent[i-5];
+	// Open file
+	FILE* file = fopen(filePath, "rb");
+	if (file == NULL) return false;
 
-		// Calculate corresponding number for code
-		uint32_t number = uint32_t(let3 - 'A') * 676000u +
-		                  uint32_t(let2 - 'A') * 26000u +
-		                  uint32_t(let1 - 'A') * 1000u +
-		                  uint32_t(no3 - '0') * 100u +
-		                  uint32_t(no2 - '0') * 10u +
-		                  uint32_t(no1 - '0');
+	// Read through the file
+	const size_t BUFFER_SIZE = 512;
+	char buffer[BUFFER_SIZE];
+	size_t readSize;
+	while ((readSize = fread(buffer, 1, BUFFER_SIZE, file)) > 0) {
+		for (size_t i = 0; i < readSize; i += 8) {
+			
+			char let3 = buffer[i];
+			char let2 = buffer[i + 1];
+			char let1 = buffer[i + 2];
+			char no3 = buffer[i + 3];
+			char no2 = buffer[i + 4];
+			char no1 = buffer[i + 5];
+
+			// Calculate corresponding number for code
+			uint32_t number = uint32_t(let3 - 'A') * 676000u +
+			                  uint32_t(let2 - 'A') * 26000u +
+			                  uint32_t(let1 - 'A') * 1000u +
+			                  uint32_t(no3 - '0') * 100u +
+			                  uint32_t(no2 - '0') * 10u +
+			                  uint32_t(no1 - '0');
+
+			uint32_t bitsetChunkIndex = number / 64;
+			uint64_t bitIndex = number % 64;
+
+			uint64_t chunk = isFoundBitset[bitsetChunkIndex];
+			uint64_t bitMask = uint64_t(1) << bitIndex;
 		
+			bool exists = (bitMask & chunk) != 0;
 
-		uint32_t bitsetChunkIndex = number / 64;
-		uint64_t bitIndex = number % 64;
+			if (exists) {
+				foundCopy = true;
+				break;
+			}
 
-		uint64_t chunk = isFoundBitset[bitsetChunkIndex];
-		uint64_t bitMask = uint64_t(1) << bitIndex;
-		
-		bool exists = (bitMask & chunk) != 0;
-
-		if (exists) {
-			foundCopy = true;
-			break;
+			chunk = bitMask | chunk;
+			isFoundBitset[bitsetChunkIndex] = chunk;
 		}
-
-		chunk = bitMask | chunk;
-		isFoundBitset[bitsetChunkIndex] = chunk;
 	}
 
-	// Free allocated memory
+	// Free allocated memory and return result
 	_aligned_free(isFoundBitset);
-	_aligned_free(fileContent);
-
 	return foundCopy;
 }
