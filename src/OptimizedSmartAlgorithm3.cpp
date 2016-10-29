a7ab2d726b67a16cd9fe06bff5e9675ec020f567
Peter Hillerström <peter@hstroem.se>
2016-10-29 11:07:11 +0200

    Bad threading

--- src/OptimizedSmartAlgorithm3.cpp
+++ src/OptimizedSmartAlgorithm3.cpp
@@ -2,6 +2,8 @@
 
 #include "OptimizedSmartAlgorithm3.hpp"
 
+#include <algorithm>
+#include <atomic>
 #include <cstdint>
 #include <cstdio>
 #include <cstring>
@@ -9,6 +11,17 @@
 #include <thread>
 
 using namespace std;
+
+// Constants
+// ------------------------------------------------------------------------------------------------
+
+#define NUM_BYTES_PER_CODE 8
+#define NUM_BITSET_BYTES 2197000
+
+#define NUM_THREADS 8
+
+#define CODE_ALLOCATION_BATCH_SIZE 8192
+
 
 // Statics
 // ------------------------------------------------------------------------------------------------
@@ -55,9 +68,64 @@ static __declspec(noalias)size_t readFile(const char* __restrict path, char* __r
 
 static __declspec(noalias) void workerFunction(const char* __restrict file,
                                                uint64_t* __restrict isFoundBitset,
-                                               bool* __restrict foundCopy) noexcept
+                                               bool* __restrict foundCopy,
+                                               size_t numCodes,
+                                               atomic_size_t* nextFreeCodeIndex) noexcept
 {
 	// Clear bitset
+	memset(isFoundBitset, 0, NUM_BITSET_BYTES);
+
+	while (true) {
+
+		// Allocate codes from shared array
+		size_t codeIndex = atomic_fetch_add(nextFreeCodeIndex, CODE_ALLOCATION_BATCH_SIZE);
+		if (codeIndex >= numCodes) return;
+		size_t codesToCheck = min(size_t(CODE_ALLOCATION_BATCH_SIZE), numCodes - codeIndex);
+		
+		// Loop over all allocated codes
+		size_t start = codeIndex * NUM_BYTES_PER_CODE;
+		size_t end = (codeIndex + codesToCheck) * NUM_BYTES_PER_CODE;
+		for (size_t i = start; i < end; i += NUM_BYTES_PER_CODE) {
+			
+			char let3 = file[i];
+			char let2 = file[i + 1];
+			char let1 = file[i + 2];
+			char no3 = file[i + 3];
+			char no2 = file[i + 4];
+			char no1 = file[i + 5];
+
+			// Calculate corresponding number for code
+			uint32_t number = uint32_t(let3 - 'A') * 676000u +
+			                  uint32_t(let2 - 'A') * 26000u +
+			                  uint32_t(let1 - 'A') * 1000u +
+			                  uint32_t(no3 - '0') * 100u +
+			                  uint32_t(no2 - '0') * 10u +
+			                  uint32_t(no1 - '0');
+
+			uint32_t bitsetChunkIndex = number / 64;
+			uint64_t bitIndex = number % 64;
+
+			uint64_t chunk = isFoundBitset[bitsetChunkIndex];
+			uint64_t bitMask = uint64_t(1) << bitIndex;
+		
+			bool exists = (bitMask & chunk) != 0;
+
+			if (exists) {
+
+				// Allocate rest of rays so the other threads can stop
+				atomic_fetch_add(nextFreeCodeIndex, numCodes);
+
+				// Signal that the copy is found and exit thread
+				*foundCopy = true;
+				return;
+			}
+
+			chunk = bitMask | chunk;
+			isFoundBitset[bitsetChunkIndex] = chunk;
+		}
+	}
+
+/*	// Clear bitset
 	const size_t NUM_BITSET_BYTES = 2197000;
 	memset(isFoundBitset, 0, NUM_BITSET_BYTES);
 
@@ -103,7 +171,7 @@ static __declspec(noalias) void workerFunction(const char* __restrict file,
 			chunk = bitMask | chunk;
 			isFoundBitset[bitsetChunkIndex] = chunk;
 		}
-	}
+	}*/
 }
 
 // Exposed function
@@ -112,17 +180,16 @@ static __declspec(noalias) void workerFunction(const char* __restrict file,
 bool optimizedSmartAlgorithm3(const char* filePath) noexcept
 {
 	// Read file
-	size_t tmpSize = sizeOfFile(filePath) + 16; // +16 so we guarantee SIMD readable
+	size_t tmpSize = sizeOfFile(filePath);
 	char* file = static_cast<char*>(_aligned_malloc(tmpSize, 32));
 	size_t fileSize = readFile(filePath, file);
-
-
-	const size_t NUM_THREADS = 1;
-
-	
+	size_t numCodes = fileSize / NUM_BYTES_PER_CODE;
 
 	// Variable containing whether a copy was found or not
 	bool foundCopy = false;
+
+	// Counter used for allocating codes
+	atomic_size_t nextFreeCodeIndex = 0;
 
 	// Start threads
 	uint64_t* bitsets[NUM_THREADS];
@@ -130,16 +197,39 @@ bool optimizedSmartAlgorithm3(const char* filePath) noexcept
 	for (size_t i = 0; i < NUM_THREADS; i++) {
 
 		// Allocate memory for bitset, cleared in worker function
-		const size_t NUM_BITSET_BYTES = 2197000;
 		bitsets[i] = static_cast<uint64_t*>(_aligned_malloc(NUM_BITSET_BYTES, 32));
 
 		// Start worker thread
-		threads[i] = thread(workerFunction, filePath, bitsets[i], &foundCopy);
+		threads[i] = thread(workerFunction, file, bitsets[i], &foundCopy, numCodes, &nextFreeCodeIndex);
 	}
 
 	// Wait for threads to finish working
 	for (thread& t : threads) {
 		t.join();
+	}
+
+	// Compare all threads tables
+	if (!foundCopy) {
+		uint64_t bitsetChunks[NUM_THREADS];
+
+		for (size_t i = 0; i < (NUM_BITSET_BYTES / sizeof(uint64_t)); i++) {
+			for (size_t j = 0; j < NUM_THREADS; j++) {
+				bitsetChunks[j] = bitsets[j][i];
+			}
+			
+			for (size_t j = 0; j < NUM_THREADS; j++) {
+				for (size_t k = 0; k < NUM_THREADS; k++) {
+					if (j == k) continue;
+
+					if ((bitsetChunks[j] & bitsetChunks[k]) != uint64_t(0)) {
+						foundCopy = true;
+						goto out;
+					}
+				}
+			}
+		}
+		out:
+		int a;
 	}
 
 	// Free memory
