9120c68e838834deb08e9af8144728ed9a8954b7
Peter Hillerström <peter@hstroem.se>
2016-11-19 14:30:24 +0100

    I won the contest

---
+++ i_won_page/Consid.se — Chalmers-student bäst i programmeringstävling – belönas med resa till tech-mekka_files/footer.js
@@ -0,0 +1,30 @@
+﻿$(function () {
+    $('.footerComponent-selectOffice .selectpicker').on("change", function (event) {
+        var id = $(this).val();
+
+        $.getJSON("/api/office/data",
+        { id: id }, function(data) {
+            var holder = $(".footerComponent-officeInfo");
+
+            var officeLinks = holder.find(".footerComponent-officeLink");
+
+            officeLinks.eq(0).attr("href", "tel:+46" + data.phone.substr(1).replace(" ", "").replace("-", ""));
+            officeLinks.eq(1).attr("href", "mailto:" + data.email);
+            officeLinks.eq(2)
+                .attr("href",
+                    "https://www.google.se/maps/dir//" +
+                    data.streetaddress +
+                    ",+" +
+                    data.postaladdress +
+                    "/@" +
+                    data.coord +
+                    ",16z/");
+
+            var dataHolders = holder.find(".footerComponent-officeLink .meta");
+
+            dataHolders.eq(0).text(data.phone);
+            dataHolders.eq(1).text(data.email);
+            dataHolders.eq(2).text(data.streetaddress);
+        });
+    });
+});
---
+++ i_won_page/Consid.se — Chalmers-student bäst i programmeringstävling – belönas med resa till tech-mekka_files/mailchimp.js
@@ -0,0 +1,31 @@
+﻿var accessKey;
+
+var mailchimp = {
+    initialize: function(configuration) {
+        accessKey = configuration.apiKey;
+    },
+
+    send : function(name, email) {
+        console.log(name);
+        console.log(email);
+    }
+};
+
+
+$(document).on("focus", "input[id $= 'MailChimp']", function() {
+
+    var button = $(this).closest("form").find("button[id='subscribe']");
+
+    button.removeAttr("disabled");
+
+    button.on("submit", function (e) {
+        e.preventDefault();
+        var siblings = $(this).siblings("input");
+
+        mailchimp.send(siblings.find("input[id='name'").val(), siblings.find("input[id='email']").val());
+
+        return false;
+    });
+    
+
+});
